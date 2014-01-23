e011f03d159064c06d5ee7d4d2973146be77c48b
unknown <Yauheni_Novik@EPBYGOMW0234.gomel.epam.com>
2014-01-23 18:28:38 +0300

    adding second comment

--- task1/src/main/java/com/epam/novik/task1/App.java
+++ task1/src/main/java/com/epam/novik/task1/App.java
@@ -12,5 +12,6 @@ public class App {
 		log.info("Hello World!");
 		System.out.println("adding sysout");
 		System.out.println("adding first comment into branch");
+		System.out.println("adding second comment into branch");
 	}
 }
