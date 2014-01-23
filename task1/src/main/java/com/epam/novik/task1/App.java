044c16f011b7f1e3cd45353383bf53cd40ef7bf9
unknown <Yauheni_Novik@EPBYGOMW0234.gomel.epam.com>
2014-01-23 18:26:19 +0300

    adding first comment

--- task1/src/main/java/com/epam/novik/task1/App.java
+++ task1/src/main/java/com/epam/novik/task1/App.java
@@ -11,5 +11,6 @@ public class App {
 	public static void main(String[] args) {
 		log.info("Hello World!");
 		System.out.println("adding sysout");
+		System.out.println("adding first comment into branch");
 	}
 }
