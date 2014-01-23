d49bf94a81bf1676a0d63cea051e564b34668d24
unknown <Yauheni_Novik@EPBYGOMW0234.gomel.epam.com>
2014-01-23 18:36:49 +0300

    removing first comment

--- task1/src/main/java/com/epam/novik/task1/App.java
+++ task1/src/main/java/com/epam/novik/task1/App.java
@@ -10,8 +10,7 @@ public class App {
 
 	public static void main(String[] args) {
 		log.info("Hello Yauheni Novik!");
-		System.out.println("adding sysout");
-		System.out.println("adding first comment into branch");
+		System.out.println("adding System.out.println()");
 		System.out.println("adding second comment into branch");
 		System.out.println("adding third comment into branch");
 	}
