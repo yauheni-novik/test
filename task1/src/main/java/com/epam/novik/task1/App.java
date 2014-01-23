1dbabd360c862959d3d2463073ad2b19e3dd0671
unknown <Yauheni_Novik@EPBYGOMW0234.gomel.epam.com>
2014-01-23 18:01:53 +0300

    adding project

---
+++ task1/src/main/java/com/epam/novik/task1/App.java
@@ -0,0 +1,14 @@
+package com.epam.novik.task1;
+
+/**
+ * Hello world!
+ * 
+ */
+public class App {
+	private static org.apache.log4j.Logger log = org.apache.log4j.Logger
+			.getLogger(App.class);
+
+	public static void main(String[] args) {
+		log.info("Hello World!");
+	}
+}
