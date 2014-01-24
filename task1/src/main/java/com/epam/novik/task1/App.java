9c6a6b773ae14ec99ac5ca121cb1420c78040a5d
unknown <Yauheni_Novik@EPBYGOMW0234.gomel.epam.com>
2014-01-24 13:14:47 +0300

    delete some rows

--- task1/src/main/java/com/epam/novik/task1/App.java
+++ task1/src/main/java/com/epam/novik/task1/App.java
@@ -11,7 +11,5 @@ public class App {
 	public static void main(String[] args) {
 		log.info("Hello Yauheni Novik!");
 		System.out.println("adding System.out.println()");
-		System.out.println("adding second comment into branch");
-		System.out.println("adding third comment into branch");
 	}
 }
